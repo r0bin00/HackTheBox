38afe7394c81748fe6ee4b728014b71762f1c57c
Irfanul Montasir <mdirfanmusa@gmail.com>
2020-04-13 03:53:37 +0600

    Update namescrap.py

--- namescrap.py
+++ namescrap.py
@@ -1,37 +1,21 @@
 import requests as rq
 from bs4 import BeautifulSoup as bs
 
-my_url = 'https://www.othoba.com/laptop'
+my_url = 'https://github.com/TheBinitGhimire/Web-Shells'
 
 p_html = rq.get(my_url)
 
 p_soup = bs(p_html.text,"html.parser")
 
 
-p_con = p_soup.findAll("div",{"class":"details"})
-brand_con = p_soup.findAll("span",{"class":"value"})
-price_con = p_soup.findAll("span",{"class":"price actual-price"})
+p_con = p_soup.findAll("a",{"class":"js-navigation-open"})
 
-filename = "laptop.csv"
 
-f = open(filename,"w")
+name = []
 
-headers = "Brand, Product_Name, Price\n"
-f.write(headers)
-
-product_name = []
-product_brand = []
-product_price = []
 
 for p_name in p_con:
-    product_name.append(p_name.h2.a.text)
+    name.append(p_name.a.text)
 
-for p_brand in brand_con:
-    product_brand.append(p_brand.text.strip())
-for p_price in price_con:
-    product_price.append(p_price.text)
-
-
-for i,j,k in zip(product_name, product_brand, product_price):
-    f.write(j + "," + i.replace(",","|")+","+k+ "\n")
-f.close()
+for i in name:
+    print(i + "\n")
